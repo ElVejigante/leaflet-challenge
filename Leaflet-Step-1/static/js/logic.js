a4ccb2d84cbc1ccaf3e5fed437660bcbdeebed61
ElVejigante <ernesto.cancel@gmail.com>
2021-07-08 14:09:31 -0400

    Update logic.js

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -2,22 +2,27 @@
 var queryURL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.geojson"
 
 d3.json(queryURL, function(seismicData) {
-    // Base Map
-    var myMap = L.map("map", {
-        center: [37, -95],
-        zoom: 5
-    });
-
+    
+    var quakes = L.layerGroup();
     //Title Layer
-    L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    var backMap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
         attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
         tileSize: 512,
         maxZoom: 18,
         zoomOffset: -1,
         id: "mapbox/streets-v11",
         accessToken: API_KEY
-        });
+    });
+    var baseMap = {"Light Map": backMap};
+    var overlayMap = {"Earthquakes": quakes}
     
+    // Base Map
+    var myMap = L.map("map", {
+        center: [37, -95],
+        zoom: 5,
+        layers: [backMap, quakes]
+    });
+    L.control.layers(baseMap, overlayMap, {collapsed:false}).addTo(myMap);
     function markerSize(mag) {
         return mag*5
     };
@@ -35,16 +40,5 @@ d3.json(queryURL, function(seismicData) {
                 return "yellow";
         }
     }
-    seismicData.features.forEach(function (feature) {
-        L.circle([feature.geometry.coordinates[1], feature.geometry.coordinates[0]], {
-            fillOpacity: 0.8,
-            color: "red",
-            fillColor: colors(feature.geometry.coordinates[2]),
-            radius: markerSize(feature.properties.mag)
-        }).addTo(myMap).bindPopup("<h3>" + feature.properties.place +
-            "</h3><hr><p>" + new Date(feature.properties.time) + "</p><p>"  
-            +"Magnitude: "+ feature.properties.mag + "</p><p>" 
-            +"Depth: "+ feature.geometry.coordinates[2] + "</p>")
-
-    })
+    
 })
