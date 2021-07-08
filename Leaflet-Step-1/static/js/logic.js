05faa6bea013c1baae0b186f618e216159724f5a
ElVejigante <ernesto.cancel@gmail.com>
2021-07-08 13:04:37 -0400

    Update logic.js

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -1,7 +1,7 @@
 // Seismic data url, significant earthquakes in the last month
 var queryURL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.geojson"
 
-d3.json(queryURL).then(function (seismicData, err) {
+d3.json(queryURL).then(function (seismicData) {
     // Base Map
     var myMap = L.map("map", {
         center: [37, -95],
@@ -18,5 +18,35 @@ d3.json(queryURL).then(function (seismicData, err) {
         accessToken: API_KEY
         }).addTo(myMap);
     
-        
+    function markerSize(mag) {
+        return mag*3
+    };
+
+    function colors(d) {
+        if (d < 1){
+            return "#ED4311"
+          } else if ( d < 2){
+            return "#ED7211"
+          } else if (d < 3){
+            return "#EDB411"
+          } else if (d < 4){
+            return "#EDD911"
+          } else if (d < 5 ){
+            return "#DCED11"
+          } else {
+            return "#B7DF5F"
+          }   
+    };
+    earthquakeData.features.forEach(function (feature) {
+        L.circle([feature.geometry.coordinates[1], feature.geometry.coordinates[0]], {
+            fillOpacity: 0.8,
+            color: "black",
+            fillColor: colors(feature.properties.mag),
+            radius: markerSize(feature.properties.mag)
+        }).addTo(myMap).bindPopup("<h3>" + feature.properties.place +
+            "</h3><hr><p>" + new Date(feature.properties.time) + "</p><p>"  
+            +"Magnitude: "+ feature.properties.mag + "</p><p>" 
+            +"Depth: "+ feature.geometry.coordinates[2] + "</p>")
+
+    })
 })
