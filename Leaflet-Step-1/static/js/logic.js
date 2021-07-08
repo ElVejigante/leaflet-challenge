9cc7496b855f481f932db886033cd9412938fd99
ElVejigante <ernesto.cancel@gmail.com>
2021-07-08 12:13:34 -0400

    Update logic.js

---
+++ Leaflet-Step-1/static/js/logic.js
@@ -0,0 +1,6 @@
+// Seismic data url, significant earthquakes in the last month
+var queryURL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.geojson"
+
+d3.json(queryURL).then(function (seismicData, err) {
+    
+})
